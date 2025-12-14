af667724bf05f0d3549b7d640fe80fdd56ab40b9
bharadwajasb <bharadwajasb@gmail.com>
2025-12-14 22:08:36 +0530

    Added risky port checks in security groups

--- basic-sec-checker.py
+++ basic-sec-checker.py
@@ -20,22 +20,21 @@ def mfaCheck(userList):
 def securityGroupChecker():
     sgsvc = boto3.client("ec2") # Creating client for using EC2 methods
     response = sgsvc.describe_security_groups()
-    sg_ids = []
+    sg_ids, risky_rules = [],[]
+    risky_ports = [22,3389]
     for i in response['SecurityGroups']: # Adding all security group IDs to a list
         sg_ids.append(i['GroupId'])
-    for group in sg_ids: # Iterating the list of SG IDs
+    for group in sg_ids: # Iterating the list of SG IDs using SG ID
         response_rules = sgsvc.describe_security_group_rules(
-            Filters=[
-                {'Name':'group-id',
-                 'Values': [group]
-                 }
-                 ]
-                 )
-        print(response_rules)
+            Filters=[{'Name':'group-id','Values': [group]}])
+        for sgr in response_rules['SecurityGroupRules']: # Iterating each rule within each Security group 
+            if not sgr['IsEgress'] and sgr['FromPort'] in risky_ports and sgr['CidrIpv4'] and sgr['CidrIpv4']=='0.0.0.0/0': 
+                risky_rules.append(sgr['GroupId'])
+    print(f'Security groups with risky ports open are {risky_rules}')     # Printing the final security groups with risky ports open in inbound rules
 
 
 if __name__ == "__main__":
     userList = iam() # Function to get the IAM users list.
-    #mfaCheck(userList['Users']) # Sending only the value associated with Users key.
+    mfaCheck(userList['Users']) # Sending only the value associated with Users key.
     securityGroupChecker()
 
