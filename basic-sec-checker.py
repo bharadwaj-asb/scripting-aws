c0053e4bffbf65b4866b8831936dcfb27f022075
bharadwajasb <bharadwajasb@gmail.com>
2025-12-14 19:01:16 +0530

    Added security group logic

--- basic-sec-checker.py
+++ basic-sec-checker.py
@@ -1,22 +1,41 @@
 import boto3
 
-svc = boto3.client("iam")
+iamsvc = boto3.client("iam") # Creating client for using IAM methods
+
 def iam():
-    response = svc.list_users()
-    #print(response)
+    response = iamsvc.list_users() # Returns list of users in a dict 
     return response
-    #for user in response["Users"]:
-     #   print(user["UserName"])
+
+
 def mfaCheck(userList):
     response = []
-    for value in userList:
-        mfaStatus = svc.list_mfa_devices(UserName=value['UserName'])
+    for value in userList: # For each value in the dict
+        mfaStatus = iamsvc.list_mfa_devices(UserName=value['UserName'])  # Getting the dict associated with UserName
         if mfaStatus['MFADevices'] != []:
             print(f'MFA is enabled for {value['UserName']}')
         else:
             print(f'MFA is not enabled for {value['UserName']}')
-        
+    # Yet to be paginated for larger response    
+
+def securityGroupChecker():
+    sgsvc = boto3.client("ec2") # Creating client for using EC2 methods
+    response = sgsvc.describe_security_groups()
+    sg_ids = []
+    for i in response['SecurityGroups']: # Adding all security group IDs to a list
+        sg_ids.append(i['GroupId'])
+    for group in sg_ids: # Iterating the list of SG IDs
+        response_rules = sgsvc.describe_security_group_rules(
+            Filters=[
+                {'Name':'group-id',
+                 'Values': [group]
+                 }
+                 ]
+                 )
+        print(response_rules)
+
 
 if __name__ == "__main__":
-    userList = iam()
-    mfaCheck(userList['Users'])
+    userList = iam() # Function to get the IAM users list.
+    #mfaCheck(userList['Users']) # Sending only the value associated with Users key.
+    securityGroupChecker()
+
